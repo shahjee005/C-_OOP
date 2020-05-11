cbeb6a3f8aec12e1b6503604ce0ba4d5009f0926
Syed-Gardezi <61136718+shahjee005@users.noreply.github.com>
2020-05-11 10:48:36 -0600

    added inhertience

---
+++ inheritance.cs
@@ -0,0 +1,27 @@
+﻿// using System;
+
+// namespace inheritance 
+// {
+// class Group  //created a new group instead of Class
+//     {
+//         public string groupName= "Hikers R US";
+//         public void greet(){
+//             Console.WriteLine("Welcome Back!");
+//         }
+//         class User:Group
+//         {
+//             public string userName="JohnDoe";
+//         }
+
+
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             User newUser= new User();
+//             newUser.greet();
+//             Console.WriteLine(newUser.userName+ " is a member of "+ newUser.groupName);
+//         }
+//     }
+// }
+// }
